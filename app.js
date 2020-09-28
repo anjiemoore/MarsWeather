534edad8098aae59782b233e4d3cc54cbbc31430
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-28 00:38:07 -0600

    returning sol object and finished getting all the info needed for application

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const API_KEY = 'DEMO_KEY'
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
 
-getWeather();
+getWeather().then(sols => {
+    console.log(sols)
+});
 
 function getWeather() {
-    fetch(API_URL).then(res => res.json()).then(data => {
+    return fetch(API_URL).then(res => res.json()).then(data => {
         const {
             sol_keys,
             validity_checks,
             ...solData
         } = data
-        console.log(solData)
+        return Object.entries(solData).map(([sol, data]) => {
+            return {
+                sol: sol,
+                minTemp: data.AT.mn,
+                maxTemp: data.AT.mx,
+                solDate: new Date(data.First_UTC),
+                solSeason: data.Season,
+                solWind: data.HWS.av,
+                solWindDir: data.WD.most_common.compass_point
+            }
+        })
     });
 }
