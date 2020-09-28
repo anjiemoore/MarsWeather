a180d3218d76410f8e0ec4765d24eee2e7016b6d
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-28 00:14:39 -0600

    getting only individual sol data with deconstructor and spread operators

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const API_KEY = 'DEMO_KEY'
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
 
-getWeather()
+getWeather();
 
 function getWeather() {
     fetch(API_URL).then(res => res.json()).then(data => {
-        console.log(data)
+        const {
+            sol_keys,
+            validity_checks,
+            ...solData
+        } = data
+        console.log(solData)
     });
 }
