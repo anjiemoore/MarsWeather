c3b6a37702a752ad6f27ffd2c184f32cdb04f293
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-10-05 11:12:24 -0600

    ran lighthouse checks and everything is green, yeah!

--- app.js
+++ app.js
@@ -5,25 +5,25 @@ getWeather().then(sols => {
     console.log(sols)
 });
 
-function getWeather() {
-    return fetch(API_URL).then(res => res.json()).then(data => {
-        const {
-            sol_keys,
-            validity_checks,
-            ...solData
-        } = data
-        return Object.entries(solData).map(([sol, data]) => {
-            return {
-                sol: sol,
-                minTemp: data.AT.mn,
-                maxTemp: data.AT.mx,
-                solDate: new Date(data.First_UTC),
-                solSeason: data.Season,
-                // solWindSpeed: data.HWS.av,
-                // solWindDir: data.WD.most_common.compass_point,
-                solSunrise: new Date(data.First_UTC),
-                solSunset: new Date(data.First_UTC)
-            }
-        })
+async function getWeather() {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    const {
+        sol_keys,
+        validity_checks,
+        ...solData
+    } = data;
+    return Object.entries(solData).map(([sol, data_2]) => {
+        return {
+            sol: sol,
+            minTemp: data_2.AT.mn,
+            maxTemp: data_2.AT.mx,
+            solDate: new Date(data_2.First_UTC),
+            solSeason: data_2.Season,
+            // solWindSpeed: data.HWS.av,
+            // solWindDir: data.WD.most_common.compass_point,
+            solSunrise: new Date(data_2.First_UTC),
+            solSunset: new Date(data_2.First_UTC)
+        };
     });
 }
